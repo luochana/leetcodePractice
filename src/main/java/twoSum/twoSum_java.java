ebe43eda401017b4a90308241d3f2dd9e7041397
luochana <1329127421@qq.com>
2019-07-13 01:17:43 +0800

    add something new

---
+++ src/main/java/twoSum/twoSum_java.java
@@ -0,0 +1,27 @@
+package twoSum;
+
+public class twoSum_java {
+    public int[] two_Sum(int[] nums, int target) {
+        int len=nums.length;
+        int[] b=new int[2];
+        for(int i=0;i<len;++i)
+            for(int j=0;j<len;++j)
+            {
+                if (nums[i]+nums[j]==target)
+                {
+                    b[0]=i;
+                    b[1]=j;
+                    break;
+                }
+            }
+        return b;
+    }
+
+    public static void main(String[] args) {
+        int b[]={2,7,11,15};
+        twoSum_java temp=new twoSum_java();
+        int result[]=temp.two_Sum(b,9);
+        for(int i=0;i<result.length;++i)
+            System.out.println(result[i]);
+    }
+}
