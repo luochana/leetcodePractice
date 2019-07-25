b64a1e13513a2b700a796cd865d1354f7b910c6e
luochana <1329127421@qq.com>
2019-07-25 15:50:34 +0800

    add something new

---
+++ src/main/java/longestPalindrome/longestPalindrome_scala.scala
@@ -0,0 +1,34 @@
+package longestPalindrome
+
+object longestPalindrome_scala {
+
+  def longestPalindrome(s:String):String={
+    if (s == null || s.length() < 1)
+      return ""
+    var start=0
+    var end=0
+    for(i<-0 until(s.length)){
+      val len1 = expandAroundCenter(s, i, i)
+      val len2 = expandAroundCenter(s, i, i + 1)
+      val len = Math.max(len1, len2)
+      if (len > end - start) {
+        start = i - (len - 1) / 2
+        end = i + len / 2
+      }
+    }
+    return s.substring(start, end + 1)
+  }
+
+  def  expandAroundCenter(s:String,left:Int,right:Int):Int={
+
+    var L = left
+    var R = right
+    while ( {
+      L >= 0 && R < s.length && s.charAt(L) == s.charAt(R)
+    }) {
+      L -= 1
+      R += 1
+    }
+    return R - L - 1
+  }
+}
