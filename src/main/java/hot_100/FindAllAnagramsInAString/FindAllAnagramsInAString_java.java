98b1907f5286ec519a8171566682c4a142bc3d5a
luochana <1329127421@qq.com>
2019-08-22 00:51:15 +0800

    add something new

---
+++ src/main/java/hot_100/FindAllAnagramsInAString/FindAllAnagramsInAString_java.java
@@ -0,0 +1,29 @@
+package hot_100.FindAllAnagramsInAString;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindAllAnagramsInAString_java {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        char [] sChar = s.toCharArray();
+        char [] pChar = p.toCharArray();
+        int [] curAToZ = new int[26];
+        int [] aToZ = new int[26];
+        for (char c : pChar) {
+            aToZ[c - 'a']++;
+        }
+        for (int i = 0; i < sChar.length; i++) {
+            if (i >= pChar.length) {
+                curAToZ[sChar[i-pChar.length] - 'a']--;
+            }
+            curAToZ[sChar[i] - 'a']++;
+            if (Arrays.equals(curAToZ, aToZ)) {
+                result.add(i - pChar.length + 1);
+            }
+        }
+        return result;
+
+    }
+}
---
+++ src/main/java/hot_100/FindAllAnagramsInAString/FindAllAnagramsInAString_scala.scala
@@ -0,0 +1,28 @@
+package hot_100.FindAllAnagramsInAString
+
+import java.util
+import java.util.{ArrayList, Arrays, List}
+
+class FindAllAnagramsInAString_scala {
+  def findAnagrams(s: String, p: String):ArrayList[Integer] = {
+    val result = new ArrayList[Integer]
+    val sChar = s.toCharArray
+    val pChar = p.toCharArray
+    val curAToZ = new Array[Int](26)
+    val aToZ = new Array[Int](26)
+    for (c <- pChar) {
+      aToZ(c - 'a') += 1
+    }
+
+    for ( i <- 0 until( sChar.length))
+    {
+      if (i >= pChar.length)
+        curAToZ(sChar(i - pChar.length) - 'a') -= 1
+      curAToZ(sChar(i) - 'a') += 1
+      if (util.Arrays.equals(curAToZ, aToZ))
+        result.add(i - pChar.length + 1)
+    }
+    result
+  }
+
+}
