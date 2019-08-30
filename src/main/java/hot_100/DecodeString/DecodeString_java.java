d3cb80d1c0430612e2cab57bab942c0f9ccd0b77
luochana <1329127421@qq.com>
2019-08-30 12:37:15 +0800

    add something new

--- src/main/java/hot_100/DecodeString/DecodeString_java.java
+++ src/main/java/hot_100/DecodeString/DecodeString_java.java
@@ -3,6 +3,17 @@ package hot_100.DecodeString;
 import java.util.LinkedList;
 
 public class DecodeString_java {
+    /*
+     * 解题思路：
+     * 将decodeStr拆解后会有四种可能：数字、字母、[、]
+     * 使用栈保存遍历的结果，numStack保存数字，stringStack保存字母
+     * 例:3[a2[c]]
+     * 1、遇到数字：计算数字的大小，注意连续数字的情况
+     * 2、遇到左括号：将之前得到的数字入栈，之前得到的字母也入栈，情况数字和字母
+     * 3、遇到字母：累加连续字母
+     * 4、遇到右括号：将数字出栈，将累加字母根据数字翻倍，将字母也出栈，和翻倍字母拼接
+     * 5、循环1-4
+     */
     public String decodeString(String s) {
         StringBuilder builder = new StringBuilder();
         LinkedList<Integer> numStack = new LinkedList<>();
---
+++ src/main/java/hot_100/TopKFrequentElements/TopKFrequentElements_scala.scala
@@ -0,0 +1,5 @@
+package hot_100.TopKFrequentElements
+
+class TopKFrequentElements_scala {
+
+}
