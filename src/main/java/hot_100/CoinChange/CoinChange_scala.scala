2a77a1ffcd43f3dcb8e08f3ce16b6cbb6de59624
luochana <1329127421@qq.com>
2019-09-01 22:45:56 +0800

    add something new

---
+++ src/main/java/hot_100/CoinChange/CoinChange_scala.scala
@@ -0,0 +1,20 @@
+package hot_100.CoinChange
+
+import java.util.Arrays
+
+class CoinChange_scala {
+  def coinChange(coins: Array[Int], amount: Int): Int = {
+    val max = amount + 1
+    val dp = new Array[Int](amount + 1)
+    Arrays.fill(dp, max)
+    dp(0) = 0
+    for ( i <- 1 to amount) {
+      for (j <- 0 until  coins.length) {
+        if (coins(j) <= i)
+          dp(i) = Math.min(dp(i), dp(i - coins(j)) + 1)
+      }
+    }
+    if (dp(amount) > amount) -1
+    else dp(amount)
+  }
+}
