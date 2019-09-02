b0fb48682286ac4309e99e1a1476b542c9eb540d
luochana <1329127421@qq.com>
2019-09-02 21:28:22 +0800

    add something new

---
+++ src/main/java/hot_100/BestTimeToBuyandSellStockWithCooldown/BestTimeToBuyandSellStockWithCooldown_java.java
@@ -0,0 +1,31 @@
+package hot_100.BestTimeToBuyandSellStockWithCooldown;
+
+public class BestTimeToBuyandSellStockWithCooldown_java {
+    public int maxProfit(int[] prices) {
+        if(prices.length == 0) {
+            return 0;
+        }
+        //dp1[i]表示第i天手上有股票的最大收益（前i天的最后一个状态是买入）
+        int[] dp1 = new int[prices.length];
+        //dp2[i]表示第i天卖出股票的最大收益
+        int[] dp2 = new int[prices.length];
+        //dp3[i]表示前i天最后一个状态为冷冻期的最大收益（第i天要么无状态即冷冻期后没有再买入，要么为冻结状态）
+        int[] dp3 = new int[prices.length];
+
+        dp1[0] = 0 - prices[0];
+        for(int i = 1; i < prices.length; i++) {
+            //第i天买入股票：dp3[i - 1] - prices[i]（dp3[i - 1]表明今天可以买）
+            //第i天不买入股票：dp1[i - 1]
+            dp1[i] = Math.max(dp3[i - 1] - prices[i], dp1[i - 1]);
+            //第i天卖出股票
+            dp2[i] = dp1[i - 1] + prices[i];
+            //第i天为冷冻期：代表第i-1天卖出股票->dp2[i - 1]
+            //第i天为无状态：dp3[i- 1]
+            dp3[i] = Math.max(dp2[i - 1], dp3[i - 1]);
+        }
+        //1.最后一天卖出
+        //2.最后一天为冷冻期
+        //3.最后一天无状态（冷冻期后没有再买入）
+        return Math.max(dp2[prices.length - 1], dp3[prices.length - 1]);
+    }
+}
