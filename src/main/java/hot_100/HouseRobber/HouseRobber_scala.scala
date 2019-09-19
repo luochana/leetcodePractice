0be180c669745ef10d58eeb814a9400bc5069e49
luochana <1329127421@qq.com>
2019-09-19 10:59:39 +0800

    add something new

---
+++ src/main/java/hot_100/HouseRobber/HouseRobber_scala.scala
@@ -0,0 +1,19 @@
+package hot_100.HouseRobber
+
+class HouseRobber_scala {
+  def rob(nums: Array[Int]): Int = {
+    val length = nums.length
+    if (length == 0) return 0
+    if (length == 1) return nums(0)
+    val dp = new Array[Int](length)
+    if (length >= 2) {
+      for(i <- 0 until(length))
+        dp(i) = nums(i)
+      dp(1) = Math.max(dp(0), dp(1))
+      for(i <- 2 until(length)) {
+        dp(i) = Math.max(dp(i - 2) + nums(i), dp(i - 1))
+      }
+    }
+    dp(length - 1)
+  }
+}
