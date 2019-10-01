b2b8a22ec5633a72223eff0a3206356eb356f72f
luochana <1329127421@qq.com>
2019-10-01 13:30:47 +0800

    add something new

---
+++ src/main/java/hot_100/BinaryTreeMaximumPathSum/BinaryTreeMaximumPathSum_java.java
@@ -0,0 +1,58 @@
+package hot_100.BinaryTreeMaximumPathSum;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+ }
+
+public class BinaryTreeMaximumPathSum_java {
+
+    //理解错题意了,注意是路径不是子树
+/*    int max=Integer.MIN_VALUE;
+    int tempMax=0;
+    public int maxPathSum(TreeNode root) {
+        SequentialTraversal(root);
+        return max;
+    }
+
+    public void SequentialTraversal(TreeNode node){
+        if(node.left!=null)
+            SequentialTraversal(node.left);
+
+        if(node.right!=null)
+            SequentialTraversal(node.right);
+
+        if(tempMax>0)
+            tempMax+=node.val;
+        else
+            tempMax=node.val;
+        max=Math.max(max,tempMax);
+    }*/
+
+    int max_sum = Integer.MIN_VALUE;
+
+    public int max_gain(TreeNode node) {
+        if (node == null) return 0;
+
+        // max sum on the left and right sub-trees of node
+        int left_gain = Math.max(max_gain(node.left), 0);
+        int right_gain = Math.max(max_gain(node.right), 0);
+
+        // the price to start a new path where `node` is a highest node
+        int price_newpath = node.val + left_gain + right_gain;
+
+        // update max_sum if it's better to start a new path
+        max_sum = Math.max(max_sum, price_newpath);
+
+        // for recursion :
+        // return the max gain if continue the same path
+        return node.val + Math.max(left_gain, right_gain);
+    }
+
+    public int maxPathSum(TreeNode root) {
+        max_gain(root);
+        return max_sum;
+    }
+}
---
+++ src/main/java/hot_100/BinaryTreeMaximumPathSum/BinaryTreeMaximumPathSum_scala.scala
@@ -0,0 +1,24 @@
+package hot_100.BinaryTreeMaximumPathSum
+
+class BinaryTreeMaximumPathSum_scala {
+  private var max_sum = Integer.MIN_VALUE
+
+  def max_gain(node: TreeNode): Int = {
+    if (node == null) return 0
+    // max sum on the left and right sub-trees of node
+    val left_gain = Math.max(max_gain(node.left), 0)
+    val right_gain = Math.max(max_gain(node.right), 0)
+    // the price to start a new path where `node` is a highest node
+    val price_newpath = node.`val` + left_gain + right_gain
+    // update max_sum if it's better to start a new path
+    max_sum = Math.max(max_sum, price_newpath)
+    // for recursion :
+    // return the max gain if continue the same path
+    node.`val` + Math.max(left_gain, right_gain)
+  }
+
+  def maxPathSum(root: TreeNode): Int = {
+    max_gain(root)
+    max_sum
+  }
+}
