61c29b805d52fa9eea15e72373596695acf53e51
luochana <1329127421@qq.com>
2019-10-09 00:42:19 +0800

    add something new

---
+++ src/main/java/hot_100/BinaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal_java.java
@@ -0,0 +1,44 @@
+package hot_100.BinaryTreeLevelOrderTraversal;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+ }
+public class BinaryTreeLevelOrderTraversal_java {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res= new ArrayList<List<Integer>>();
+        if (root == null)
+            return res;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        int level = 0;
+        while(!queue.isEmpty()){
+            // start the current level
+            res.add(new ArrayList<Integer>());
+            // number of elements in the current level
+            int level_length = queue.size();
+            for(int i = 0; i < level_length; ++i) {
+                TreeNode node = queue.remove();
+
+                // fulfill the current level
+                res.get(level).add(node.val);
+
+                // add child nodes of the current level
+                // in the queue for the next level
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            // go to next level
+            level++;
+        }
+        return res;
+    }
+}
---
+++ src/main/java/hot_100/BinaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal_scala.scala
@@ -0,0 +1,31 @@
+package hot_100.BinaryTreeLevelOrderTraversal
+
+import java.util.{ArrayList, LinkedList, List, Queue}
+
+class BinaryTreeLevelOrderTraversal_scala {
+
+  def levelOrder(root: TreeNode): List[List[Integer]] = {
+    val res = new ArrayList[List[Integer]]
+    if (root == null) return res
+    val queue = new LinkedList[TreeNode]
+    queue.add(root)
+    var level = 0
+    while (!queue.isEmpty) { // start the current level
+      res.add(new ArrayList[Integer])
+      // number of elements in the current level
+      val level_length = queue.size
+      for(i <- 0 until( level_length)) {
+        val node = queue.remove
+        // fulfill the current level
+        res.get(level).add(node.`val`)
+        // add child nodes of the current level
+        // in the queue for the next level
+        if (node.left != null) queue.add(node.left)
+        if (node.right != null) queue.add(node.right)
+      }
+      // go to next level
+      level += 1
+    }
+    res
+  }
+}
