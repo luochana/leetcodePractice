c068cef1ebf50fed0ece33015d5f478a3e67e0d6
luochana <1329127421@qq.com>
2019-10-30 00:13:13 +0800

    add something new

---
+++ src/main/java/hot_100/SearchInRotatedSortedArray/SearchInRotatedSortedArray_java.java
@@ -0,0 +1,36 @@
+package hot_100.SearchInRotatedSortedArray;
+
+public class SearchInRotatedSortedArray_java {
+
+    //一趟搞定，旋转后，一边符合升序，一边不符合，利用此特点
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        int mid = left + (right-left)/2;
+
+        while(left <= right){
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(nums[left] <= nums[mid]){  //左边升序
+                if(target >= nums[left] && target <= nums[mid]){//在左边范围内
+                    right = mid-1;
+                }else{//只能从右边找
+                    left = mid+1;
+                }
+
+            }else{ //右边升序
+                if(target >= nums[mid] && target <= nums[right]){//在右边范围内
+                    left = mid +1;
+                }else{//只能从左边找
+                    right = mid-1;
+                }
+
+            }
+            mid = left + (right-left)/2;
+        }
+
+        return -1;  //没找到
+    }
+}
---
+++ src/main/java/hot_100/SearchInRotatedSortedArray/SearchInRotatedSortedArray_scala.scala
@@ -0,0 +1,5 @@
+package hot_100.SearchInRotatedSortedArray
+
+class SearchInRotatedSortedArray_scala {
+
+}
