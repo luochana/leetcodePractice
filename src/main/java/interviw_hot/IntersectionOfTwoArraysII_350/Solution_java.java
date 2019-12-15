e8e60a4208e1ddc9975f70007e1b561d3ec76f81
luochana <1329127421@qq.com>
2019-12-15 22:23:28 +0800

    add something new

---
+++ src/main/java/interviw_hot/IntersectionOfTwoArraysII_350/Solution_java.java
@@ -0,0 +1,28 @@
+package interviw_hot.IntersectionOfTwoArraysII_350;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution_java {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        for(int num : nums1) {
+            if(!map.containsKey(num)) map.put(num, 1);
+            else map.put(num, map.get(num) + 1);
+        }
+        for(int num : nums2) {
+            if(map.containsKey(num)) {
+                map.put(num, map.get(num) - 1);
+                if(map.get(num) == 0) map.remove(num);
+                list.add(num);
+            }
+        }
+        int[] res = new int[list.size()];
+        for(int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+}
