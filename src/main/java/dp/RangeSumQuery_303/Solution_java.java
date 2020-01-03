7270a11ff1a8dc4259fdcbe7d5d3fdb9f6122f0c
luochana <1329127421@qq.com>
2020-01-04 02:19:40 +0800

    add something new

---
+++ src/main/java/dp/RangeSumQuery_303/Solution_java.java
@@ -0,0 +1,16 @@
+package dp.RangeSumQuery_303;
+
+public class Solution_java {
+    private int[] sum;
+
+    public Solution_java(int[] nums) {
+        sum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i];
+    }
+}
