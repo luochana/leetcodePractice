4a0c0121a23e2c1677b4a5c621163d1f70532a42
luochana <1329127421@qq.com>
2020-06-08 06:45:11 +0800

    add something new

---
+++ src/main/java/BinarySearch/SingleElementInASortedArray_540/Solution_java.java
@@ -0,0 +1,20 @@
+package BinarySearch.SingleElementInASortedArray_540;
+
+public class Solution_java {
+    
+    public int singleNonDuplicate(int[] nums) {
+        int l = 0, r = nums.length;
+        while(l < r){
+            int m = l + (r -l) / 2;
+            if(m % 2 == 1){
+                --m;
+            }
+            if(nums[m] == nums[m + 1]){
+                l = m + 2;
+            }else{
+                r = m;
+            }
+        }
+        return nums[l];
+    }
+}
