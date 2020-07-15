01e92395a81a6c19e90a8be7b583a2eed160afaf
luochana <1329127421@qq.com>
2020-07-16 01:24:39 +0800

    add something new

---
+++ src/main/java/math/Base_7_504/Solution_java.java
@@ -0,0 +1,20 @@
+package math.Base_7_504;
+
+public class Solution_java {
+    public String convertToBase7(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        boolean isNegative = num < 0;
+        if (isNegative) {
+            num = -num;
+        }
+        while (num > 0) {
+            sb.append(num % 7);
+            num /= 7;
+        }
+        String ret = sb.reverse().toString();
+        return isNegative ? "-" + ret : ret;
+    }
+}
