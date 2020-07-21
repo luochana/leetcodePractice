6a37d0fa3cb902d6da944f055c0d1b45d7242774
luochana <1329127421@qq.com>
2020-07-21 23:48:07 +0800

    add something new

--- src/main/java/math/MinimumMovesToEqualArrayElementsII_462/Solution_java.java
+++ src/main/java/math/MinimumMovesToEqualArrayElementsII_462/Solution_java.java
@@ -1,4 +1,17 @@
 package math.MinimumMovesToEqualArrayElementsII_462;
 
+import java.util.Arrays;
+
 public class Solution_java {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int move = 0;
+        int l = 0, h = nums.length - 1;
+        while (l <= h) {
+            move += nums[h] - nums[l];
+            l++;
+            h--;
+        }
+        return move;
+    }
 }
